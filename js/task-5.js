e3d26f5797c4190ddb2ea6ac8fd37162e3492981
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-24 17:23:00 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,63 +1,32 @@
-class Car {
-  // Write code under this line
-  static getSpecs(car) {
-    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`;
-  }
-  constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) {
-    this.speed = speed;
-    this._price = price;
-    this.maxSpeed = maxSpeed;
-    this.isOn = isOn;
-    this.distance = distance;
-  }
-  get price() {
-    return this._price;
-  }
-  set price(value) {
-    this._price = value;
-  }
-  turnOn() {
-    this.isOn = true;
-  }
-  turnOff() {
-    this.isOn = false;
-    this.speed = 0;
-  }
-  accelerate(value) {
-    if (this.speed + value <= this.maxSpeed) {
-      this.speed += value;
-    } else {
-      this.speed = this.maxSpeed;
-    }
-  }
-  decelerate(value) {
-    if (this.speed - value >= 0) {
-      this.speed -= value;
-    } else {
-      this.speed = 0;
-    }
-  }
-  drive(hours) {
-    if (this.isOn) {
-      this.distance += hours * this.speed;
-    }
-  }
-}
-const mustang = new Car({ maxSpeed: 200, price: 2000 });
-mustang.turnOn();
-mustang.accelerate(50);
-mustang.drive(2);
-//
-console.log(Car.getSpecs(mustang));
-// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+// Write code under this line
+const getUserWithEmail = (array, mail) =>
+  array.find(({ email }) => email === mail);
+console.log(getUserWithEmail(users, "rossvazquez@xinware.com"));
 
-mustang.decelerate(20);
-mustang.drive(1);
-mustang.turnOff();
+/* {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+} */
 
-console.log(Car.getSpecs(mustang));
-// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+// console.log(getUserWithEmail(users, 'blackburndotson@furnigeer.com'));
 
-console.log(mustang.price); // 2000
-mustang.price = 4000;
-console.log(mustang.price); // 4000
+/* {
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+} */
