454c8c51e416e0c1e20cad4a48600eaf14e5774d
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-24 17:07:52 +0300

    Create task-3.js

---
+++ js/task-3.js
@@ -0,0 +1,50 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    this.items = this.items.filter((itemName) => itemName !== item);
+  }
+}
+
+console.log(typeof Storage);
+// 'function'
+
+const goods = ["Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор"];
+
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem("Дроид");
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem("Пролонгер");
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
