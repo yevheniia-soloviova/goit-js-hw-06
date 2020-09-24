594e84f8fd880ac602cd755cb31cb5cd30312ad2
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-24 17:22:48 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,13 @@
-// Write code under this line
-const Account = function (login, email) {
-  this.login = login;
-  this.email = email;
-};
-Account.prototype.getInfo = function () {
-  return `login : ${this.login}, email: ${this.email}`;
-};
-console.log(typeof Account.prototype.getInfo);
-// 'function'
+const getUserNames = (users) => users.map(({ name }) => name);
 
-const mango = new Account("Mangozedog", "mango@dog.woof");
-console.log(mango.getInfo());
-// 'login : Mangozedog, email: mango@dog.woof'
+console.log(getUserNames(users));
 
-const poly = new Account("Poly", "poly@mail.com");
-console.log(poly.getInfo());
-// 'login : Poly, email: poly@mail.com'
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
