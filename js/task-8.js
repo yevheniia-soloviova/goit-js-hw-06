45413b0e6cf3230e97cdd66f5a79a21ce466d62d
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-09-24 17:38:32 +0300

    Update task-8.js

---
+++ js/task-8.js
@@ -0,0 +1,11 @@
+// Write code under this line
+const getUsersWithFriend = (array, friendName) =>
+  array
+    .filter(({ friends }) => friends.includes(friendName))
+    .map(({ name }) => name);
+
+console.log(getUsersWithFriend(users, "Briana Decker"));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+
+console.log(getUsersWithFriend(users, "Goldie Gentry"));
+// [ 'Elma Head', 'Sheree Anthony' ]
